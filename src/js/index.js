bed014c804304771be79c1e9471e01fbdf0ee966
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-07-11 13:38:33 +0300

    feat: implement correct style buttons after page reload

--- src/js/index.js
+++ src/js/index.js
@@ -53,44 +53,90 @@ if (!getFromStorage(`languageForSearch`)) {
     setToStorage(`languageForSearch`, `en`);
 }
 
+let currentLanguage = `en`;
+if (getFromStorage(`languageForSearch`)) {
+    currentLanguage = getFromStorage(`languageForSearch`);
+}
 
-const switchLanguageToEn = document.querySelector(`.button-en`);
+const buttonWithCurrentLanguage = document.querySelector(`.buttons-panel__button-${currentLanguage}`);
+buttonWithCurrentLanguage.classList.add(`active`);
+
+
+
+let currentTemperatureMeasure = `c`;
+if (getFromStorage(`typeOfTemperature`)) {
+    currentTemperatureMeasure = getFromStorage(`typeOfTemperature`);
+}
+
+console.log(currentTemperatureMeasure.toUpperCase());
+
+const buttonWithCurrentMeasure = document.querySelector(`.switchTemperatureTo${currentTemperatureMeasure.toUpperCase()}`);
+buttonWithCurrentMeasure.classList.add(`active`);
+
+
+
+
+
+
+
+
+
+
+
+const switchLanguageToEn = document.querySelector(`.buttons-panel__button-en`);
+const switchLanguageToRu = document.querySelector(`.buttons-panel__button-ru`);
+const switchLanguageToUk = document.querySelector(`.buttons-panel__button-uk`);
+
+
 switchLanguageToEn.addEventListener(`click`, () => {
     setToStorage(`languageForSearch`, `en`);
     getWeatherInfo(null, null, `en`);
     getCityName();
+    switchLanguageToEn.classList.add(`active`);
+    switchLanguageToRu.classList.remove(`active`);
+    switchLanguageToUk.classList.remove(`active`);
 })
 
-const switchLanguageToRu = document.querySelector(`.button-ru`);
+
 switchLanguageToRu.addEventListener(`click`, () => {
     setToStorage(`languageForSearch`, `ru`);
     getWeatherInfo(null, null, `ru`);
-    translate(`ru`);
     getCityName();
+    switchLanguageToRu.classList.add(`active`);
+    switchLanguageToEn.classList.remove(`active`);
+    switchLanguageToUk.classList.remove(`active`);
 })
 
-const switchLanguageToUk = document.querySelector(`.button-uk`);
+
 switchLanguageToUk.addEventListener(`click`, () => {
     setToStorage(`languageForSearch`, `uk`);
     getWeatherInfo(null, null, `uk`);
-    translate(`uk`);
     getCityName();
+    switchLanguageToUk.classList.add(`active`);
+    switchLanguageToRu.classList.remove(`active`);
+    switchLanguageToEn.classList.remove(`active`);
 })
 
+
 const switchTemperatureToF = document.querySelector(`.switchTemperatureToF`);
+const switchTemperatureToC = document.querySelector(`.switchTemperatureToC`);
+
 switchTemperatureToF.addEventListener(`click`, () => {
     setToStorage(`typeOfTemperature`, `f`);
     switchTemperature(`f`);
+    switchTemperatureToF.classList.add(`active`);
+    switchTemperatureToC.classList.remove(`active`);
 })
 
-const switchTemperatureToC = document.querySelector(`.switchTemperatureToC`);
+
 switchTemperatureToC.addEventListener(`click`, () => {
     setToStorage(`typeOfTemperature`, `c`);
     switchTemperature(`c`);
+    switchTemperatureToC.classList.add(`active`);
+    switchTemperatureToF.classList.remove(`active`);
 })
 
-const refreshButton = document.querySelector(`.refresh`);
+const refreshButton = document.querySelector(`.buttons-panel__refresh`);
 refreshButton.addEventListener(`click`, () => {
     setBackgroundImage();
 })
-
