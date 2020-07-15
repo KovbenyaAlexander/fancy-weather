feec94f571918f42e691cb2c07d6d49d39c7a90c
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-07-15 21:06:52 +0300

    deploy

--- src/js/storage.js
+++
@@ -1,7 +0,0 @@
-export function setToStorage(name, value) {
-    window.localStorage.setItem(name, JSON.stringify(value));
-}
-
-export function getFromStorage(name) {
-    return JSON.parse(window.localStorage.getItem(name));
-}
